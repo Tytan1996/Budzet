0e5e337b8315e0a439fe9d903b290e130e66a75e
Maciej Pestka <maciekpestka1990@gmail.com>
2022-03-12 21:16:57 +0100

    utworzono metody klasy

--- include/Wydatki.h
+++ include/Wydatki.h
@@ -8,6 +8,14 @@ using namespace std;
 class Wydatki
 {
     public:
+        void ustawIdWydatku(int nowyIdWydatku);
+        void ustawNazweWydatku(string nowaNazwaWydatku);
+        void ustawDateWydatku(string nowaDataWydatku);
+        void ustawKwotewydatku(float nowaKwotaWydatku);
+        int pobierzIdWydatku();
+        string pobierzNazweWydatku();
+        string pobierzDateWydatku();
+        float pobierzKwoteWydatku();
 
     protected:
 
