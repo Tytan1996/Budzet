8d85b07649685e68bbcfae54f1258b734e4d012c
Maciej Pestka <maciekpestka1990@gmail.com>
2022-03-19 08:04:10 +0100

    skonczenie calej klasy

--- include/Dochod.h
+++ include/Dochod.h
@@ -1,16 +1,32 @@
 #ifndef DOCHOD_H
 #define DOCHOD_H
 
+#include <iostream>
+
+using namespace std;
 
 class Dochod
 {
     public:
-        Dochod();
-        virtual ~Dochod();
+        void ustawIdDochodu(int nowyIdDochodu);
+        void ustawIdUzytkownika(int nowyIdUzytkownika);
+        void ustawNazweDochodu(string nowaNazwaDochodu);
+        void ustawDateDochodu(string nowaDataDochodu);
+        void ustawKwoteDochodu(float nowaKwotaDochodu);
+        int pobierzIdDochodu();
+        int pobierzIdUzytkownika();
+        string pobierzNazweDochodu();
+        string pobierzDateDochodu();
+        float pobierzKwoteDochodu();
 
     protected:
 
     private:
+        int idDochodu;
+        int idUzytkownika;
+        string nazwaDochodu;
+        string dataDochodu;
+        float kwotaDochodu;
 };
 
 #endif // DOCHOD_H
--- src/Dochod.cpp
+++ src/Dochod.cpp
@@ -1,11 +1,32 @@
 #include "Dochod.h"
 
-Dochod::Dochod()
-{
-    //ctor
+void Dochod::ustawIdDochodu(int nowyIdDochodu){
+    idDochodu=nowyIdDochodu;
 }
-
-Dochod::~Dochod()
-{
-    //dtor
+void Dochod::ustawIdUzytkownika(int nowyIdUzytkownika){
+    idUzytkownika=nowyIdUzytkownika;
+}
+void Dochod::ustawNazweDochodu(string nowaNazwaDochodu){
+    nazwaDochodu=nowaNazwaDochodu;
+}
+void Dochod::ustawDateDochodu(string nowaDataDochodu){
+    dataDochodu=nowaDataDochodu;
+}
+void Dochod::ustawKwoteDochodu(float nowaKwotaDochodu){
+    kwotaDochodu=nowaKwotaDochodu;
+}
+int Dochod::pobierzIdDochodu(){
+    return idDochodu;
+}
+int Dochod::pobierzIdUzytkownika(){
+    return idUzytkownika;
+}
+string Dochod::pobierzNazweDochodu(){
+    return nazwaDochodu;
+}
+string Dochod::pobierzDateDochodu(){
+    return dataDochodu;
+}
+float Dochod::pobierzKwoteDochodu(){
+    return kwotaDochodu;
 }
