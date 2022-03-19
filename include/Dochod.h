a7f64e6dff2f41be9263eabddbd5ff75c581fc01
Maciej Pestka <maciekpestka1990@gmail.com>
2022-03-19 07:59:11 +0100

    utworzonio klasę

---
+++ include/Dochod.h
@@ -0,0 +1,16 @@
+#ifndef DOCHOD_H
+#define DOCHOD_H
+
+
+class Dochod
+{
+    public:
+        Dochod();
+        virtual ~Dochod();
+
+    protected:
+
+    private:
+};
+
+#endif // DOCHOD_H
---
+++ src/Dochod.cpp
@@ -0,0 +1,11 @@
+#include "Dochod.h"
+
+Dochod::Dochod()
+{
+    //ctor
+}
+
+Dochod::~Dochod()
+{
+    //dtor
+}
